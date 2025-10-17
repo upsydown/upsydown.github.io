3a31ee4ddb76cfeff1286801bbd26954959c776f
TonNom <ton@email>
2025-10-17 14:59:43 +0200

    Deploy: update site with latest source changes

---
+++ _next/static/HDPCXg1JpdX3vc4c6lmR_/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/b637e9a5-6cf11adc33c7fdd4.js","static/chunks/921-f03f0ca0b1746e3a.js","static/chunks/pages/index-9cebb00118d46504.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
